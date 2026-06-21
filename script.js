0bd073befdba4b1a523381d5d4fbc5fd37e1b24a
Learning <snehadani635@gmail.com>
2026-06-21 20:26:39 +0530

    add

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+const accessKey = "L7BH1CTcIHYz_5-Wl9pyvQr1GD_1rYIBzB36og5ARCg";
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
