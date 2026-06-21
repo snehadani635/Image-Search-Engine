1cbf2749eb8b9f80f5f78da8864d1646d9d712d2
Learning <snehadani635@gmail.com>
2026-06-21 20:39:06 +0530

    add

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let page = 1;
 
 async function searchImages(){
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
